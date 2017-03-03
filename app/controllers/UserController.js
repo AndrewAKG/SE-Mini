1fd667559fb20cc31919af469ba787fdaeec32a4
TheDreamHunter <andrew.kamal@student.guc.edu.eg>
2017-03-03 02:09:19 +0200

    Na2s pagination bs with Exceptions Handling

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -5,111 +5,157 @@ var userSession;
 
 
 let userController = {
-    
-//User Adding Links
-    addURL:function(req, res){
-        let userN = userSession.UserName; 
 
-        User.findOne({UserName:userN},function(err, user){
-            if(err){
-                res.send(err.message)
-                console.log(err);
-            }
-            else{
-             user.Links.push(req.body);
-             user.save(function(err){});
-                console.log(req.body);
-                res.redirect('/Home');
-            }
-        })
-    },
-//User Adding ScreenShots  
-    addScreenShot:function(req, res){
-        let userN = userSession.UserName; 
-        let pic = req.file.originalname ;;
-    
-        User.findOne({UserName:userN},function(err, user){
-            if(err){
-                res.send(err.message)
-                console.log(err);
-            }
-            else{
-           user.ScreenShots.push({title : req.body.title,Pic : pic});
-           user.save(function(err){});
+    //User Adding Links
+    addURL: function (req, res) {
+        if (req.body.title == "" || req.body.link == "") {
+            let userN = userSession.UserName;
+            User.findOne({ UserName: userN }, function (err, user) {
 
-                console.log(user);
-                res.redirect('/Home');
-            }
-        })
+                if (err)
+                    res.send(err.message);
+                else {
+                    res.render('HomeView', { user, empty: 1 });
+                }
+            })
+        }
+        else {
+            let userN = userSession.UserName;
+            User.findOne({ UserName: userN }, function (err, user) {
+                if (err) {
+                    res.send(err.message)
+                    console.log(err);
+                }
+                else {
+                    user.Links.push(req.body);
+                    user.save(function (err) { });
+                    console.log(req.body);
+                    res.redirect('/Home');
+                }
+            })
+        }
     },
-//User SignUp
-    createUser:function(req, res){
-        let user = new User(req.body);
-       user.Image = req.file.originalname ;
-        user.save(function(err, user){
-            if(err){
-                res.send(err.message)
-                console.log(err);
-            }
-            else{           
-                userSession=req.session;
-        userSession.UserName=req.body.UserName;     
-                console.log(user);
-                res.redirect('/AddWork');
-            }
-        })
+    //User Adding ScreenShots  
+    addScreenShot: function (req, res) {
+        if (!(req.file) || req.body.title == "") {
+            let userN = userSession.UserName;
+            User.findOne({ UserName: userN }, function (err, user) {
+
+                if (err)
+                    res.send(err.message);
+                else {
+                    res.render('HomeView', { user, empty: 2 });
+                }
+            })
+        } else {
+            let userN = userSession.UserName;
+            let pic = req.file.originalname;
+            User.findOne({ UserName: userN }, function (err, user) {
+                if (err) {
+                    res.send(err.message)
+                    console.log(err);
+                }
+                else {
+                    user.ScreenShots.push({ title: req.body.title, Pic: pic });
+                    user.save(function (err) { });
+
+                    console.log(user);
+                    res.redirect('/Home');
+                }
+            })
+        }
     },
-//User AddingWork
-    gotoAddWork:function(req,res){
-        let userN = userSession.UserName; 
-        User.findOne({UserName:userN},function(err, user){
-            
-            if(err)
+    //User SignUp
+    createUser: function (req, res) {
+        if ((req.body.UserName == "") || (req.body.PassWord == "") || (req.body.FirstName == "") || (req.body.LastName == "")) {
+            res.render('SignUpView', { empty: 1 })
+        }
+        else {
+            let user = new User(req.body);
+            if (req.file) {
+
+                user.Image = req.file.originalname;
+                user.save(function (err, user) {
+                    if (err) {
+                        res.send(err.message)
+                        console.log(err);
+                    }
+                    else {
+                        userSession = req.session;
+                        userSession.UserName = req.body.UserName;
+                        console.log(user);
+                        res.redirect('/AddWork');
+                    }
+                })
+            } else {
+                user.save(function (err, user) {
+                    if (err) {
+                        res.send(err.message)
+                        console.log(err);
+                    }
+                    else {
+                        userSession = req.session;
+                        userSession.UserName = req.body.UserName;
+                        console.log(user);
+                        res.redirect('/AddWork');
+                    }
+                })
+            }
+        }
+    },
+    //User AddingWork
+    gotoAddWork: function (req, res) {
+        let userN = userSession.UserName;
+        User.findOne({ UserName: userN }, function (err, user) {
+
+            if (err)
                 res.send(err.message);
-            else{
-            res.render('HomeView',{user});
-        }    
-    })
+            else {
+                res.render('HomeView', { user, empty: 0 });
+            }
+        })
     },
-//User Portfolio Page
-    getUser:function(req, res){
-        let userN = userSession.UserName; 
-        User.findOne({UserName:userN},function(err, user){
-            
-            if(err)
+    //User Portfolio Page
+    getUser: function (req, res) {
+        let userN = userSession.UserName;
+        User.findOne({ UserName: userN }, function (err, user) {
+
+            if (err)
                 res.send(err.message);
             else
-            res.render('PortfolioPage',{user});
+                res.render('PortfolioPage', { user });
         })
     },
-//Guest Login
-    gotoGuestHome:function(req, res){
-        User.find(function(err, users){
-            
-            if(err)
+    //Guest Login
+    gotoGuestHome: function (req, res) {
+        User.find(function (err, users) {
+
+            if (err)
                 res.send(err.message);
             else
-                res.render('GuestHome',{users});
+                res.render('GuestHome', { users });
         })
     },
-//Login Check
-     checkUser:function(req, res){
-
-       User.findOne({UserName:req.body.UserName,PassWord:req.body.PassWord},function(err,user){
-       if(err){
-        res.send(err.message);
-       }
-       else if(!user) {
-       res.send("balabizooooooo");
-       }
-       else{
-            userSession=req.session;
-            userSession.UserName=req.body.UserName;
-            res.redirect('/Home');
-       }
-    })
-       }
-    
+    //Login Check
+    checkUser: function (req, res) {
+        if (req.body.UserName == "" || req.body.PassWord == "") {
+            res.render('loginView', { UserNotFound: 2 })
+        } else {
+            User.findOne({ UserName: req.body.UserName, PassWord: req.body.PassWord }, function (err, user) {
+                if (err) {
+                    res.send(err.message);
+                }
+                else if (!user) {
+                    res.render('loginView', { UserNotFound: 1 });
+                }
+                else {
+                    userSession = req.session;
+                    userSession.UserName = req.body.UserName;
+                    res.redirect('/Home');
+                }
+            })
+        }
+    }
 }
 
 module.exports = userController;
--- app/routes.js
+++ app/routes.js
@@ -10,7 +10,7 @@ var storage = multer.diskStorage({
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname)
-  }
+    }
 })
 var storage2 = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -18,30 +18,30 @@ var storage2 = multer.diskStorage({
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname)
-  }
+    }
 })
 
 var upload = multer({ storage: storage })
-var upload2 = multer({ storage2: storage2 })
+var upload2 = multer({ storage: storage2 })
 
 // get requests 
 //router.get('/', projectController.getAllProjects);
-router.get('/',function(req,res){
-    res.render('index2');
+router.get('/', function (req, res) {
+    res.render('loginView', { UserNotFound: 0 });
 })
-router.get('/SignUp',function(req,res){
-    res.render('SignUpView');
+router.get('/SignUp', function (req, res) {
+    res.render('SignUpView', { empty: 0 });
 })
 router.get('/Home', UserController.getUser);
-router.get('/Guest',UserController.gotoGuestHome);
-router.get('/AddWork',UserController.gotoAddWork);
+router.get('/Guest', UserController.gotoGuestHome);
+router.get('/AddWork', UserController.gotoAddWork);
 
 //post requests
 //router.post('/SignUp', projectController.createUser);
-router.post('/SignUp',upload.single("Image"), UserController.createUser);
+router.post('/SignUp', upload.single("Image"), UserController.createUser);
 router.post('/', UserController.checkUser);
-router.post('/URL',UserController.addURL);
-router.post('/ScreenShot',upload2.single("Pic"),UserController.addScreenShot);
+router.post('/URL', UserController.addURL);
+router.post('/ScreenShot', upload2.single("Pic"), UserController.addScreenShot);
 
 // export router
 
